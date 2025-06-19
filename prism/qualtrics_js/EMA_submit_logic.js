14724125ef44f64667730195428a7e5f8ef72fb9
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2025-06-18 19:39:38 -0500

    add qualtrics js code for prism

---
+++ prism/qualtrics_js/EMA_submit_logic.js
@@ -0,0 +1,25 @@
+Qualtrics.SurveyEngine.addOnUnload(function() {
+    var participantID = "${e://Field/ParticipantID}";
+    var subjectName = "${e://Field/SubjectName}";
+    var xhr = new XMLHttpRequest();
+    var url = "https://intense-katydid-ruling.ngrok-free.app/EMA/submit";
+    
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.setRequestHeader("ngrok-skip-browser-warning", "true");
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("EMA submission successful:", xhr.responseText);
+        } else if (xhr.readyState === 4) {
+            console.error("EMA submission failed. Status:", xhr.status);
+        }
+    };
+    
+    var data = JSON.stringify({
+        "participantID": participantID,
+        "subjectName": subjectName
+    });
+    
+    xhr.send(data);
+});
