c0ab96dbf3255f9b40c7def205a9d31999733f76
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2025-07-17 14:48:03 -0500

    add duplicate check in check system and change print statements to transcript

--- prism/src/system_tasks/_check_system.py
+++ prism/src/system_tasks/_check_system.py
@@ -10,16 +10,17 @@ from system_tasks._push_data_to_research_drive import PushDataToResearchDrive
 class CheckSystem(SystemTask):
     def run(self):
         self.task_type = "CHECK_SYSTEM"
-        print(f"{self.task_type} #{self.task_number} initiated.")
+        self.app.add_to_transcript(f"{self.task_type} #{self.task_number} initiated.")
         file_system_check = self.check_file_system()
         qualtrics_check = self.check_qualtrics()
         followmee_check = self.check_followmee()
         package_check = self.check_installed_packages()
         research_drive_check = self.check_research_drive()
-        return file_system_check + qualtrics_check + followmee_check + package_check + research_drive_check
+        participant_check = self.check_participants()
+        return file_system_check + qualtrics_check + followmee_check + package_check + research_drive_check + participant_check
     
     def check_installed_packages(self):
-        print(f"INFO: Now checking installed packages...")
+        self.app.add_to_transcript(f"INFO: Now checking installed packages...")
         try:
             with open("../requirements.txt", "r") as f:
                 requirements = f.readlines()
@@ -29,20 +30,20 @@ class CheckSystem(SystemTask):
                     try:
                         __import__(req)
                     except ImportError:
-                        print(f"WARNING: The package '{req}' is not installed. Attempting to install it...")
+                        self.app.add_to_transcript(f"WARNING: The package '{req}' is not installed. Attempting to install it...")
                         try:
                             subprocess.check_call([sys.executable, "-m", "pip", "install", req])
-                            print(f"INFO: Successfully installed '{req}'.")
+                            self.app.add_to_transcript(f"INFO: Successfully installed '{req}'.")
                         except subprocess.CalledProcessError as install_error:
-                            print(f"ERROR: Failed to install '{req}'. {install_error}")
+                            self.app.add_to_transcript(f"ERROR: Failed to install '{req}'. {install_error}")
                             return 1
             return 0
         except Exception as e:
-            print(f"ERROR: {e}")
+            self.app.add_to_transcript(f"ERROR: {e}")
             return 1
 
     def check_file_system(self):
-        print(f"INFO: Now checking file system...")
+        self.app.add_to_transcript(f"INFO: Now checking file system...")
         try:
             directories = [
                 '../config',
@@ -69,20 +70,20 @@ class CheckSystem(SystemTask):
 
             for index, (directory, files_list) in enumerate(zip(directories, files)):
                 if not os.path.exists(directory):
-                    print(f"ERROR: The '{directory}' directory is missing.")
+                    self.app.add_to_transcript(f"ERROR: The '{directory}' directory is missing.")
                 for file in files_list:
                     file_path = os.path.join(directory, file)
                     if not os.path.isfile(file_path):
-                        print(f"ERROR: The file '{file_path}' is missing.")
+                        self.app.add_to_transcript(f"ERROR: The file '{file_path}' is missing.")
 
         except Exception as e:
-            print(f"ERROR: {e}")
+            self.app.add_to_transcript(f"ERROR: {e}")
             return 1
 
         return 0
     
     def check_qualtrics(self):
-        print(f"INFO: Now checking Qualtrics connection...")
+        self.app.add_to_transcript(f"INFO: Now checking Qualtrics connection...")
         survey_id = self.app.ema_survey_id
         data_center = self.app.qualtrics_data_center
         api_token = self.app.qualtrics_api_token
@@ -92,14 +93,14 @@ class CheckSystem(SystemTask):
             response = requests.get(url, headers = headers, timeout = 10)
             if response.status_code == 200:
                 return 0
-            print(f"ERROR: Status code: {response.status_code}")
+            self.app.add_to_transcript(f"ERROR: Status code: {response.status_code}")
             return 1
         except (ConnectionError, Timeout) as e:
-            print(f"ERROR: Connection error occurred: {str(e)}")
+            self.app.add_to_transcript(f"ERROR: Connection error occurred: {str(e)}")
             return 1
         
     def check_followmee(self):
-        print(f"INFO: Now checking FollowMee connection...")
+        self.app.add_to_transcript(f"INFO: Now checking FollowMee connection...")
         username = self.app.followmee_username
         api_key = self.app.followmee_api_token
         url = f"https://www.followmee.com/api/info.aspx?key={api_key}&username={username}&function=devicelist"
@@ -107,20 +108,41 @@ class CheckSystem(SystemTask):
             response = requests.get(url, timeout = 10)
             if response.status_code == 200:
                 return 0
-            print(f"ERROR: FollowMee connection failed. Status code: {response.status_code}")
+            self.app.add_to_transcript(f"ERROR: FollowMee connection failed. Status code: {response.status_code}")
             return 1
         except (ConnectionError, Timeout) as e:
-            print(f"ERROR: FollowMee connection error occurred: {str(e)}")
+            self.app.add_to_transcript(f"ERROR: FollowMee connection error occurred: {str(e)}")
             return 1
         
     def check_research_drive(self):
         if self.app.mode == "prod":
-            print(f"INFO: Now checking Research Drive connection...")
+            self.app.add_to_transcript(f"INFO: Now checking Research Drive connection...")
             researchdrivetest = PushDataToResearchDrive(self.app)
             researchdrivetest.task_type = "PUSH_DATA_TO_RESEARCH_DRIVE"
             result = researchdrivetest.map_network_drive()
             if result != 0:
-                print("ERROR: Failed to connect to Research Drive.")
+                self.app.add_to_transcript("ERROR: Failed to connect to Research Drive.")
                 return 1
-            print("INFO: Successfully connected to Research Drive.")
+            self.app.add_to_transcript("INFO: Successfully connected to Research Drive.")
+        return 0
+    
+    def check_participants(self):
+        self.app.add_to_transcript("INFO: Now checking participants...")
+        
+        # unique id check
+        participants = self.app.participant_manager.get_participants()
+        unique_ids = {}
+        for p in participants:
+            unique_id = p.get('unique_id')
+            if unique_id:
+                if unique_id in unique_ids:
+                    unique_ids[unique_id].append(p['first_name'] + " " + p['last_name'])
+                else:
+                    unique_ids[unique_id] = [p['first_name'] + " " + p['last_name']]
+        duplicates = {uid: names for uid, names in unique_ids.items() if len(names) > 1}
+        if duplicates:
+            self.app.add_to_transcript("ERROR: Duplicate unique IDs found among participants:")
+            for uid, names in duplicates.items():
+                self.app.add_to_transcript(f"Unique ID: {uid}, Participants: {', '.join(names)}")
+            return 1
         return 0
