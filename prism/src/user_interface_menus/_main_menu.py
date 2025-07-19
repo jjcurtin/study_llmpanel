477b63bed8e88c071e529ce9a2cd9b3861760201
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2025-07-19 12:05:18 -0500

    make main menu easier to use

--- prism/src/user_interface_menus/_main_menu.py
+++ prism/src/user_interface_menus/_main_menu.py
@@ -6,6 +6,7 @@ from user_interface_menus._system_task_menu import system_task_menu
 from user_interface_menus._participant_management_menus import participant_management_menu
 from user_interface_menus._log_menu import log_menu
 from user_interface_menus._shutdown_menu import shutdown_menu
+from user_interface_menus._help_menu import help_menu
 
 def main_menu(self):
     def exit_interface(self):
@@ -13,20 +14,23 @@ def main_menu(self):
         exit(0)
 
     menu_options = {
-        '1': {'description': 'Get Status and Check System', 'menu_caller': system_check_menu},
-        '2': {'description': 'Manage System Tasks and R Scripts', 'menu_caller': system_task_menu},
-        '3': {'description': 'Manage Participants', 'menu_caller': participant_management_menu},
-        '4': {'description': 'View Logs', 'menu_caller': log_menu},
-        '5': {'description': 'Shutdown PRISM', 'menu_caller': shutdown_menu},
-        '6': {'description': 'Exit', 'menu_caller': exit_interface}
+        'check': {'description': 'Get Status and Check System', 'menu_caller': system_check_menu},
+        'tasks': {'description': 'Manage System Tasks and R Scripts', 'menu_caller': system_task_menu},
+        'participants': {'description': 'Manage Participants', 'menu_caller': participant_management_menu},
+        'logs': {'description': 'View Logs', 'menu_caller': log_menu},
+        'shutdown': {'description': 'Shutdown PRISM', 'menu_caller': shutdown_menu},
+        'exit': {'description': 'Exit PRISM User Interface', 'menu_caller': exit_interface},
+        'help': {'description': 'Help', 'menu_caller': help_menu}
     }
 
     while True:
         clear()
-        print("PRISM Interface Menu:")
+        print("=" * 60)
+        print(" " * 20 + "PRISM Interface Menu")
+        print("=" * 60)
         for key, item in menu_options.items():
-            print(f"{key}: {item['description']}")
-        choice = input("Enter your choice: ").strip()
+            print(f"{key:<20}{item['description']:<20}")
+        choice = input("\nEnter your choice: ").strip()
         selected = menu_options.get(choice)
         if selected:
             handler = selected['menu_caller']
