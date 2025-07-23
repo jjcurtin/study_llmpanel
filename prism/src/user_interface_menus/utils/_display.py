2b6de0b4ad11a6a828f7f813e16ec0bb134b5864
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2025-07-22 20:43:02 -0500

    display color functions instead of manually adding escape characters every time

--- prism/src/user_interface_menus/utils/_display.py
+++ prism/src/user_interface_menus/utils/_display.py
@@ -7,48 +7,49 @@ def clear():
 
 # ------------------------------------------------------------
 
-def error(message = "An unexpected error occurred."):
+def green(message = None):
     from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        print(f"\033[31mError\033[0m: {message}")
-    else:
-        print(f"Error: {message}")
+    green, color_end = ("\033[32m", "\033[0m") if COLOR_ON else ("", "")
+    return f"{green}{message}{color_end}"
+
+def red(message = None):
+    from user_interface_menus._menu_helper import COLOR_ON
+    red, color_end = ("\033[31m", "\033[0m") if COLOR_ON else ("", "")
+    return f"{red}{message}{color_end}"
+
+def yellow(message = None):
+    from user_interface_menus._menu_helper import COLOR_ON
+    yellow, color_end = ("\033[33m", "\033[0m") if COLOR_ON else ("", "")
+    return f"{yellow}{message}{color_end}"
+
+def cyan(message = None):
+    from user_interface_menus._menu_helper import COLOR_ON
+    cyan, color_end = ("\033[36m", "\033[0m") if COLOR_ON else ("", "")
+    return f"{cyan}{message}{color_end}"
+
+def error(message = "An unexpected error occurred."):
+    print(f"{red("Error")}: {message}")
     exit_menu()
 
 def success(message = "Operation completed successfully."):
-    from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        print(f"\033[32mSuccess\033[0m: {message}")
-    else:
-        print(f"Success: {message}")
+    print(f"{green("Success")}: {message}")
     exit_menu()
     
 def exit_menu():
-    from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        input("\n\033[33mENTER to Continue> \033[0m")
-    else:
-        input("\nENTER to Continue> ")
+    input(f"\n{yellow("ENTER to Continue> ")}")
 
 def exit_interface(self):
-    from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        print(f"\033[32mExiting PRISM Interface.\033[0m")
-    else:
-        print("Exiting PRISM Interface.")
+    print(green("Exiting PRISM Interface."))
     exit(0)
 
 # ------------------------------------------------------------
 
 def print_menu_header(title):
     clear()
-    from user_interface_menus._menu_helper import WINDOW_WIDTH, COLOR_ON
+    from user_interface_menus._menu_helper import WINDOW_WIDTH
     padding = (WINDOW_WIDTH - len(title)) // 2
     print_equals()
-    if COLOR_ON:
-        print(" " * padding + f"\033[31m{title}\033[0m")
-    else:
-        print(" " * padding + title)
+    print(" " * padding + f"{red(title)}")
     print_equals()
     print()
 
@@ -63,26 +64,11 @@ def print_equals():
 # ------------------------------------------------------------
 
 def print_fixed_terminal_prompt():
-    from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        choice = input("\n\033[36mprism> \033[0m").strip()
-    else:
-        choice = input("\nprism> ").strip()
-    return choice
+    return input(f"\n{cyan("prism> ")}").strip()
 
 def print_assistant_terminal_prompt():
-    from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        choice = input("\n\033[31massistant> \033[0m").strip()
-    else:
-        choice = input("\nassistant> ").strip()
-    return choice
+    return input(f"\n{red("assistant> ")}").strip()
 
 def print_twilio_terminal_prompt():
     print("Please enter your message below. Press ENTER to send.")
-    from user_interface_menus._menu_helper import COLOR_ON
-    if COLOR_ON:
-        choice = input("\n\033[32mtwilio> \033[0m").strip()
-    else:
-        choice = input("\ntwilio> ").strip()
-    return choice
+    return input(f"\n{green("twilio> ")}").strip()
