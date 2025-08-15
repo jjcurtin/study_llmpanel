54145b4f71adb10ccc56a6a2c0756daaa77f539f
maggardcolin@gmail.com <110071999+maggardcolin@users.noreply.github.com>
2025-08-15 16:16:01 -0500

    docs

--- prism/src/user_interface_menus/help/_research_assistant_documentation.py
+++ prism/src/user_interface_menus/help/_research_assistant_documentation.py
@@ -5,38 +5,43 @@ from user_interface_menus._menu_helper import *
 def research_assistant_documentation(self):
     def getting_started(self):
         infopage(self, content = [
-            f"The following API keys are required to be in the api/ folder and have the described format for PRISM to function correctly:",
-            f'1. {green("qualtrics.api")}: "api_token","datacenter","ema_survey_id","feedback_survey_id"',
-            f'2. {green("followmee.api")}: "username","api_token"',
-            f'3. {green("twilio.api")}: "account_sid","auth_token","from_number"',
-            f'4. {green("research_drive.api")}: "destination_path","drive_letter","network_domain","network_username","wisc_netid","wisc_password"',
-            f'5. {green("ngrok.api")}: "auth_token","domain"'
+            f"PRISM is a research study tool that allows you to manage study participant metadata and communication.",
+            f"It also provides functionality for scheduling code scripts as needed for research studies.",
+            f"It is designed to run 24/7 and execute study responsibilities in real time.",
+            f"\nTo get started with PRISM, ensure the following folders and files exist.",
+            f"\n{green("api")}\n{green("azure.api")}\n{green("followmee.api")}\n{green("ngrok.api")}\n{green("qualtrics.api")}\n{green("research_drive.api")}\n{green("twilio.api")}",
+            f"\n{green("config")}\n{green("study_participants.csv")}\n{green("study_coordinators.csv")}\n{green("system_task_schedule.csv")}\n{green("script_pipeline.csv")} if using script pipelines (deprecated) \n",
+            f"To run R scripts, place them in the {green("scripts")} directory and they will be automatically detected."
         ], title = "help ra getting started")
 
     def navigation(self):
         infopage(self, content = [
-            f"PRISM is designed to be navigated through commands. You can use {yellow('command <query>')} or {yellow('?<query>')} to search for commands.",
-            f"You can look at all of the commands by typing {yellow('command')} or {yellow('?')} with no arguments.",
-            f"\nYou can use most global commands from any menu, but keep in mind that local commands take priority.",
-            f"Additionally, most local commands are only available in the menu they are defined in (e.g. most of the docs).",
-            f"Local commands are the ones you see on any given screen besides the command menu, which is accessed by {yellow('command')}.",
+            f"To navigate PRISM, you can either traverse the menus using the options displayed on each screen, or you can use commands.",
+            f"The commands are designed to allow you to access submenus faster, and there is a minimal scripting language to allow for more complex actions.",
+            f"You can use these commands in any submenu, but the options displayed in each submenu are specific to that submenu if they are not registered as a command.",
+            f"\nYou can use the {yellow('command')} or {yellow('?')} command to search for commands.",
+            f"You can also append a search query using the form {yellow('command <query>')} or {yellow('?<query>')}.",
+        ], title = "help ra navigation")
+
+    def navigation_advanced():
+        infopage(self, content = [
             f"\nSome actions are mapped to multiple commands; this is to allow for flexibility.",   
             f"For example, you can use {yellow('tasks add rscript')} or {yellow('add rscript')} to schedule an R script.",
             f"The first shows the menus you need to go through, while the second is more intuitive.",
-            f"\nYou can also use the {yellow('register')} command to save a command chain and then use it later.",
-            f"You can also use the command of the form {yellow('$<identifier> = <command_chain>')} to register a command chain.",
-            f"You can also use the command of the form {yellow('-<identifier> = <command_chain>')} to remove a saved command chain.",
-            f"You can also use the command of the form {yellow('!<identifier>')} to search for saved command chains.",
+            f"\nYou can also use the {yellow('register')} command to save a command chain (the saved version is called a macro) and then use it later.",
+            f"You can also use the command of the form {yellow('$<identifier> = <command_chain>')} to register a macro.",
+            f"You can use the command of the form {yellow('-<identifier> = <command_chain>')} to remove a saved macro.",
+            f"You can use the command of the form {yellow('!<identifier>')} to search for saved macros.",
             f"Command chains follow the format of {yellow('/<command1>?<input1>/<command2>?<input2>...')}.",
             f"You can put commands/inputs in any order, any saved chains will expand in place, and inputs associate with the command to their left."
-        ], title = "help ra navigation")
+        ], title = "help ra navigation advanced")
 
     def terminals(self):
         infopage(self, content = [
             f"PRISM has four terminal prompts:",
             f"{cyan('prism> ')}: The main prompt for PRISM. You can use this to run commands.",
-            f"{red('assistant> ')}: The prompt for the PRISM Assistant. You can use this to ask questions in natural language.",
-            f"{green('twilio> ')}: The prompt for the Twilio interface. Input from this is sent as a text message.",
+            f"{red('assistant> ')}: The prompt for the PRISM Assistant. You can use this to ask questions in natural language. The prefixes {yellow('@')} and {yellow('assistant')} will also query the assistant.",
+            f"{green('twilio> ')}: The prompt for the Twilio interface. Input from this is sent as a text message to study participants.",
             f"{yellow('ENTER to Continue> ')}: This prompt is used to continue after displaying a message. No commands can be run."
         ], title = "help ra terminals")
 
@@ -45,9 +50,7 @@ def research_assistant_documentation(self):
             f"PRISM has a task management system that allows you to run R scripts and other tasks.",
             f"You can use the {yellow('tasks')} command to manage tasks.",
             f"You can add, remove, and view tasks, as well as manage their execution.",
-            f"Tasks are stored in {green('config/scheduled_tasks.json')}.",
-            f"You can also use the {yellow('tasks add')} command to add a task.",
-            f"You can use the {yellow('tasks remove')} command to remove a task."
+            f"Tasks are stored in {green('config/scheduled_tasks.csv')}.",
         ], title = "help ra tasks")
 
     def participants(self):
@@ -63,9 +66,10 @@ def research_assistant_documentation(self):
     menu_options = {
         'start': {'description': 'Getting Started', 'menu_caller': getting_started},
         'navigation': {'description': 'Navigating PRISM through Commands', 'menu_caller': navigation},
+        'navigation advanced': {'description': 'Advanced Navigation Documentation', 'menu_caller': navigation_advanced},
         'terminals': {'description': 'Terminal Prompts', 'menu_caller': terminals},
-        'task_management': {'description': 'Task Management', 'menu_caller': task_management},
-        'participant_management': {'description': 'Participant Management', 'menu_caller': participants}
+        'task management': {'description': 'Task Management', 'menu_caller': task_management},
+        'participant management': {'description': 'Participant Management', 'menu_caller': participants}
     }
     while True:
         if not self.commands_queue:
