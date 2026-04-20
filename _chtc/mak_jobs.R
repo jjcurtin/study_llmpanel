796e5ed414a15d2464b57977063932f262a1114b
John Curtin <jjcurtin@wisc.edu>
2026-04-20 12:46:49 -0500

    update v11

--- _chtc/mak_jobs.R
+++ _chtc/mak_jobs.R
@@ -1,7 +1,7 @@
 # setup chtc jobs & associated files/folders
 
-library(tidyverse) 
 algorithm <- "glmnet"
+library(tidyverse) 
 source("https://github.com/jjcurtin/lab_support/blob/main/chtc/fun_make_jobs.R?raw=true")
 path_training_controls <- here::here(str_c("_chtc/training_controls_panel_", 
                                            algorithm,
