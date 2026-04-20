56f08fc8f5ca8e13545b109afcfda9a05e4d4e99
John Curtin <jjcurtin@wisc.edu>
2026-04-20 12:45:56 -0500

    update v11

--- _chtc/mak_jobs.R
+++ _chtc/mak_jobs.R
@@ -1,6 +1,9 @@
 # setup chtc jobs & associated files/folders
 
 library(tidyverse) 
+algorithm <- "glmnet"
 source("https://github.com/jjcurtin/lab_support/blob/main/chtc/fun_make_jobs.R?raw=true")
-path_training_controls <- here::here("_chtc/training_controls_panel.R")
+path_training_controls <- here::here(str_c("_chtc/training_controls_panel_", 
+                                           algorithm,
+                                           ".R"))
 make_jobs(path_training_controls, overwrite_batch = FALSE)
--- _chtc/training_controls_panel_glmnet.R
+++ _chtc/training_controls_panel_glmnet.R
@@ -1,5 +1,6 @@
 # Training controls for LLMPanel study for GLMNET
 
+# NOTES-------------------------------------------------------------------------
 # version 1: first version
 # version 2: corrected problem with feature selection for the three configurations
 # version 3: change hyperparameters to better tune models
@@ -7,25 +8,27 @@
 # version 7: switched to one hot encoding for non-binary categorical variables
 # version 8: scaled numeric predictors
 # version 9: limited scaling to numeric predictors
-# version 10: re-start with baseline glmnet 
+# version 10: dropped zero variance on message ratings
+# version 11: re-start with baseline glmnet 
 
-# NOTES------------------------------
-source("https://github.com/jjcurtin/lab_support/blob/main/format_path.R?raw=true")
 
-# SET GLOBAL PARAMETERS--------------------
-version <- "v10"
+# SET GLOBAL PARAMETERS---------------------------------------------------------
+version <- "v11"
 algorithm <- "glmnet"  # glmnet, random_forest, xgboost
 
+# CONFIGURATIONS ---------------------------------------------------------------
 # ordinal_nominal
 # ordinal = c(numeric, onehot, target)
 # nominal = c(onehot, target)
 feature_set <- c("numeric_onehot")
 
-seed_splits <- 102030
+# ML PARAMETERS ----------------------------------------------------------------
 ml_mode <- "regression"   # regression or classification
-configs_per_job <- 100 
+resample <- "none" # no resampling; regression problem
+y_col_name <- "message_rating" 
 
 # CHTC SPECIFIC CONTROLS----------------------------
+configs_per_job <- 100 # used for batching configs within CHTC jobs
 username <- "jjcurtin" # for setting staging directory (until we have group staging folder)
 stage_data <- FALSE # If FALSE .sif will still be staged, just not data_trn
 max_idle <- 1000
@@ -35,12 +38,8 @@ request_disk <- "1000MB" # <- tune the memory and disk
 want_campus_pools <- TRUE 
 want_ospool <- TRUE 
 
-# OUTCOME-------------------------------------
-y_col_name <- "message_rating" 
-
-resample <- "none" # no resampling; regression problem
-
 # CV SETTINGS---------------------------------
+seed_splits <- 102030
 cv_resample_type <- "nested" # can be boot, kfold, or nested
 cv_resample = NULL # can be repeats_x_folds (e.g., 1_x_10, 10_x_10) or number of bootstraps (e.g., 100)
 cv_inner_resample <- "2_x_5" # can also be a single number for bootstrapping (i.e., 100)
@@ -53,24 +52,26 @@ cv_name <- if_else(cv_resample_type == "nested",
                          cv_outer_resample),
                    str_c(cv_resample_type, "_", cv_resample))
 
-# STUDY PATHS----------------------------
+# BATCH INFO-------------------------------------------------------------------
+source("https://github.com/jjcurtin/lab_support/blob/main/format_path.R?raw=true")
+
 # the name of the batch of jobs to set folder name
 name_batch <- str_c("train_", version) 
 
 # the path to the batch of jobs to put the folder name
 path_batch <- format_path(str_c("llmpanel/chtc/", name_batch)) 
 
-# location of data set
+# location of data set for the batch
 path_data <- format_path("llmpanel/data_processed/")
 data_trn <- "panel_long.csv"
 
-# ALGORITHM-SPECIFIC HYPERPARAMETERS-----------
+# ALGORITHM-SPECIFIC HYPERPARAMETERS--------------------------------------------
 hp1_glmnet <- seq(0, 1, by = .1) # alpha (mixture)
 hp2_glmnet_min <- -8 # min for penalty grid - will be passed into c(0, exp(seq(min, max, length.out = out)))
 hp2_glmnet_max <- 0 # max for penalty grid
 hp2_glmnet_out <- 200 # length of penalty grid
 
-# FORMAT DATA-----------------------------------------
+# FORMAT DATA-------------------------------------------------------------------
 format_data <- function (d){
   d <- d |> 
     rename(y = message_rating, 
@@ -83,60 +84,57 @@ format_data <- function (d){
 }
   
 
-# BUILD RECIPE---------------------------------------
+# BUILD RECIPE------------------------------------------------------------------
 # Script should have a single build_recipe function to be compatible with fit script. 
 build_recipe <- function(d, config) {
   # d: (training) dataset from which to build recipe
   # config: single-row job-specific tibble
-  
-  # get relevant info from config (algorithm, feature_set, resample, under_ratio)
+
+  #----------------------------------------------------------------------------- 
+  # setup of constants ---------------------------------------------------------
   algorithm <- config$algorithm
   feature_set <- config$feature_set
-  
-  # Set recipe steps generalizable to all model configurations for GLMNET
-  # include recode of y to numeric
+
+  # for recoding in recipe below   
+  q_cols <- c(
+    "pref_legitimizing",
+    "pref_self_efficacy",
+    "pref_acknowledging",
+    "pref_value_affirmation",
+    "pref_norms"
+  )
+ 
+  #----------------------------------------------------------------------------- 
+  # Set recipe steps for all model configurations for GLMNET--------------------
   rec <- recipe(y ~ ., data = d) |> 
-    step_mutate(y = case_when(
-      y == "strongly_disagree" ~ 1,
-      y =="disagree" ~ 2,
-      y == "somewhat_disagree" ~ 3,
-      y == "neutral" ~ 4,
-      y == "somewhat_agree" ~ 5,
-      y == "agree" ~ 6,
-      y == "strongly_agree" ~ 7,
-      .default = NA_real_
-   )) |> 
-   step_rm(subid, survey_version, context)
-  
-  rec <- rec |> 
-    # consider each of these removals
-    step_rm(contains(c("identity", "text", "input")), 
-           dem_student, dem_n_household, dem_minority,
-           dem_race_multiple)
+    step_mutate(y = recode_values(
+      y,
+      "strongly_disagree" ~ 1,
+      "disagree"          ~ 2,
+      "somewhat_disagree" ~ 3,
+      "neutral"           ~ 4,
+      "somewhat_agree"    ~ 5,
+      "agree"             ~ 6,
+      "strongly_agree"    ~ 7)) |> 
+   step_rm(subid, survey_version, context, contains(c("identity", "text", "input")), 
+           dem_student, dem_n_household, dem_minority, dem_race_multiple) |> 
     step_zv(all_predictors()) |> 
     step_impute_median(all_numeric_predictors()) |>  
     step_impute_mode(all_nominal_predictors())
     
-  # dummy for all binary categorical
-  # done for all configurations
+  # dummy always used for binary categorical across all configs
   # UPDATE TO LIST ALL BINARY PREDICTORS
   rec <- rec |> 
     step_dummy(all_nominal_predictors(), one_hot = FALSE) 
    
-  #------------------------------------------------------------------------ 
-  # Configs for ordinal predictors 
+  #----------------------------------------------------------------------------- 
+  # Configs for ordinal predictors ---------------------------------------------
+  
   # handle ordinal predictors as numeric 
   if (str_detect(feature_set, "^ordinal_")) {
-    q_cols <- c(
-      "pref_legitimizing",
-      "pref_self_efficacy",
-      "pref_acknowledging",
-      "pref_value_affirmation",
-      "pref_norms"
-    )
   
     rec <- rec |>   
-      step_mutate(across(all_of(q_cols)),
+      step_mutate(across(all_of(q_cols)), # preferences
         ~ recode_values(
           .x,
           "strongly_disagree" ~ 1,
@@ -145,66 +143,85 @@ build_recipe <- function(d, config) {
           "neutral"           ~ 4,
           "somewhat_agree"    ~ 5,
           "agree"             ~ 6,
-          "strongly_agree"    ~ 7
-        )
-      ) |> 
-      step_mutate(pref_formality = case_when(
-        pref_formality == "strongly_prefer_informal" ~ 1,
-        pref_formality == "moderately_prefer_informal" ~ 2,
-        pref_formality == "slightly_prefer_informal" ~ 3,
-        pref_formality == "neutral" ~ 4,
-        pref_formality == "slightly_prefer_formal" ~ 5,
-        pref_formality == "moderately_prefer_formal" ~ 6,
-        pref_formality == "strongly_prefer_formal" ~ 7
-      )) |> 
-      step_mutate(dem_education = case_when(
-        dem_education == "high_school_or_ged" ~ 1,
-        dem_education == "some_college" ~ 2,
-        dem_education == "2-year_degree" ~ 3,
-        dem_education == "4-year_degree" ~ 4,
-        dem_education == "advanced_degree" ~ 5
-      )) |> 
-      step_mutate(dem_income = case_when(
-        dem_income == "Less than $25,000" ~ 1,
-        dem_income == "$25,000 - $37,499" ~ 2,
-        dem_income == "$37,500 - $49,999" ~ 3,
-        dem_income == "$50,000 - $74,999" ~ 4,
-        dem_income == "$75,000 - $99,999" ~ 5,
-        dem_income == "$100,000 - $149,999" ~ 6,
-        dem_income == "$150,000 - $199,999" ~ 7,
-        dem_income == "$200,000 or more" ~ 8
-      ))
+          "strongly_agree"    ~ 7)) |> 
+      step_mutate(pref_formality = recode_values(
+        pref_formality,
+        "strongly_prefer_informal"   ~ 1,
+        "moderately_prefer_informal" ~ 2,
+        "slightly_prefer_informal"   ~ 3,
+        "neutral"                    ~ 4,
+        "slightly_prefer_formal"     ~ 5,
+        "moderately_prefer_formal"   ~ 6,
+        "strongly_prefer_formal"     ~ 7)) |>
+      step_mutate(dem_education = recode_values(
+        dem_education,
+        "high_school_or_ged" ~ 1,
+        "some_college"       ~ 2,
+        "2-year_degree"      ~ 3,
+        "4-year_degree"      ~ 4,
+        "advanced_degree"    ~ 5)) |>
+      step_mutate(dem_income = recode_values(
+        dem_income,
+        "Less than $25,000"     ~ 1,
+        "$25,000 - $37,499"     ~ 2,
+        "$37,500 - $49,999"     ~ 3,
+        "$50,000 - $74,999"     ~ 4,
+        "$75,000 - $99,999"     ~ 5,
+        "$100,000 - $149,999"   ~ 6,
+        "$150,000 - $199,999"   ~ 7,
+        "$200,000 or more"      ~ 8))
   }
  
   
+  # handle ordinal predictors with onehot 
   if (str_detect(feature_set, "^onehot_")) {
     # NEED TO UPDATE
   }
+  
+  # handle ordinal predictors with target encoding
   if (str_detect(feature_set, "^target_")) {
     # NEED TO UPDATE
   }
  
   
-# --------------------------------------------------------------------------
-  # handle nominal configs
+  # ----------------------------------------------------------------------------
+  # Configs for multi-level nominal predictors ---------------------------------
   
+  # handle nominal predictors with onehot 
+  if (str_detect(feature_set, "_onehot$")) {
+    # NEED TO UPDATE
+  }
   
-# --------------------------------------------------------------------------
-  # finish last steps 
-  # add interactions 
+  # handle nominal predictors with target encoding
+  if (str_detect(feature_set, "_target$")) {
+    # NEED TO UPDATE
+  }
+  
+  # ----------------------------------------------------------------------------
+  # add interactions -----------------------------------------------------------
+  # UPDATE - DOES THIS NEED TO CHANGE BASED ON HOW WE HANDLE CATEGORICAL PREDICTORS?
   rec <- rec |>   
     step_interact(terms = ~ matches("^tone_(legitimizing|norms|value_affirmation|self_efficacy|acknowledging)$"):starts_with("dem_")) |> 
     step_interact(terms = ~ matches("^style_informal$"):starts_with("dem_"))
     step_interact(terms = ~ matches("^tone_(legitimizing|norms|value_affirmation|self_efficacy|acknowledging)$"):starts_with("pref_")) |> 
     step_interact(terms = ~ matches("^style_informal$"):starts_with("pref_")) 
-     
+  
+  # ----------------------------------------------------------------------------
+  # standardize ONLY numeric predictors -----------------------------------------------------------
+    
+  # UPDATE - HOW TO DO THIS BASED ON CONFIG
+  rec <- rec |> 
+    # consider standardizing only numeric predictors in a later version
+    step_normalize(matches(c("pref_", "dem_age", "dem_education", "dem_income"))) 
+       
+  # ----------------------------------------------------------------------------
+  # finish last steps-----------------------------------------------------------
+  
   # final steps 
   rec <- rec |> 
     # drop columns with NA values after imputation (100% NA)
     step_rm(where(~ any(is.na(.)))) |> 
-    step_nzv(all_predictors()) |>
-    # consider standardizing only numeric predictors in a later version
-    step_normalize(matches(c("pref_", "dem_age", "dem_education", "dem_income"))) 
+    step_nzv(all_predictors())
   
   return(rec)
 }
